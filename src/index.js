812b45ae7a2f8bc315ccf54f35c7102159e57b69
Rainer Simon <aboutgeo@gmail.com>
2022-01-11 16:46:10 +0100

    First prototype with polyline simplification

--- src/index.js
+++ src/index.js
@@ -1,53 +1,80 @@
-export default () => {
+import simplify from 'simplify-js';
 
-  cv['onRuntimeInitialized'] = () =>{
+/**
+ * Helper: chunks an array (i.e array to array of arrays)	
+ */
+const chunk = (array, size) => {	
+  const chunked_arr = [];	
 
-    let src = cv.imread('map');
-    //cv.resize(src, src, new cv.Size(1024, 1024));
+  let index = 0;	
+  while (index < array.length) {	
+      chunked_arr.push(array.slice(index, size + index));	
+      index += size;	
+  }	
+
+  return chunked_arr;	
+}
+
+const toAnnotation = coords => ({
+  "@context": "http://www.w3.org/ns/anno.jsonld",
+  "id": "#contour",
+  "type": "Annotation",
+  "body": [],
+  "target": {
+    "selector": [{
+      "type": "SvgSelector",
+      "value": `<svg><polygon points='${coords.map(xy => xy.join(',')).join(' ')}'></polygon></svg>`
+    }]
+  }
+});
+
+export default anno => {
+
+  cv.onRuntimeInitialized = () => {
+    console.log('Tool init');
+
+    const image = document.getElementById('map');
+
+    const src = cv.imread('map');
     
-    cv.imshow('mapOut', src);
-
-    let tool = new cv.segmentation_IntelligentScissorsMB();
+    const tool = new cv.segmentation_IntelligentScissorsMB();
     tool.setEdgeFeatureCannyParameters(32, 100);
     tool.setGradientMagnitudeMaxLimit(200);
     tool.applyImage(src);
 
     let hasMap = false;
 
-    let canvas = document.getElementById('mapOut');
-
-    canvas.addEventListener('click', e => {
-        let startX = e.offsetX, startY = e.offsetY; console.log(startX, startY);
-        if (startX < src.cols && startY < src.rows)
-        {
-            console.time('buildMap');
-            tool.buildMap(new cv.Point(startX, startY));
-            console.timeEnd('buildMap');
-            hasMap = true;
-        }
+    image.addEventListener('click', evt => {
+      const { offsetX, offsetY } = evt;
+      
+      if (offsetX < src.cols && offsetY < src.rows) {
+        console.time('Building map');
+        tool.buildMap(new cv.Point(offsetX, offsetY));
+        console.timeEnd('Building map');
+        hasMap = true;
+      }
     });
 
-    canvas.addEventListener('mousemove', e => {
-        let x = e.offsetX, y = e.offsetY; //console.log(x, y);
-        let dst = src.clone();
-        if (hasMap && x >= 0 && x < src.cols && y >= 0 && y < src.rows)
-        {
-            let contour = new cv.Mat();
-            tool.getContour(new cv.Point(x, y), contour);
-            let contours = new cv.MatVector();
-            contours.push_back(contour);
-            let color = new cv.Scalar(255, 0, 0, 255);  // RGBA
-            cv.polylines(dst, contours, false, color, 3, cv.LINE_8);
-            contours.delete(); contour.delete();
-        }
-        cv.imshow('mapOut', dst);
-        dst.delete();
-    });
-    canvas.addEventListener('dispose', e => {
-        src.delete();
-        tool.delete();
+    image.addEventListener('mousemove', e => {
+      const x = e.offsetX;
+      const y = e.offsetY; 
+
+      if (hasMap && x >= 0 && x < src.cols && y >= 0 && y < src.rows) {
+        const contour = new cv.Mat();
+        tool.getContour(new cv.Point(x, y), contour);
+
+        const points = chunk(contour.data32S, 2).map(xy => ({ x: xy[0], y: xy[1] }));
+        const simplified = simplify(points, 5, true).map(({x,y}) => [x, y]);
+
+        const asAnnotation = toAnnotation(simplified);
+        anno.addAnnotation(asAnnotation);        
+      }
     });
 
+    image.addEventListener('dispose', e => {
+      src.delete();
+      tool.delete();
+    });
   }
 
 }
