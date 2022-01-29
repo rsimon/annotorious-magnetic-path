7c67d04fe406456b622b4739a9109e40c5fc8218
Rainer Simon <aboutgeo@gmail.com>
2022-01-29 13:30:25 +0100

    Started setting up boilerplate for tool plugin

---
+++ src/MagneticPolyline.js
@@ -0,0 +1,11 @@
+export default class MagneticPolyline {
+
+  constructor(originX, originY, g, env) {
+    this.origin = [ originX, originY ];
+  }
+
+  get element() {
+    // TODO
+  }
+
+}
---
+++ src/MagneticPolylineTool.js
@@ -0,0 +1,53 @@
+import Tool from '@recogito/annotorious/src/tools/Tool';
+
+import MagneticPolyline from './MagneticPolyline';
+
+export default class MagneticPolylineTool extends Tool {
+
+  constructor(g, config, env) {
+    super(g, config, env);
+
+    // The 'rubberband' magnetic polyline
+    this.rubberband = null;
+  }
+
+  startDrawing = (x, y) => {
+    this.attachListeners({
+      mouseMove: this.onMouseMove,
+      mouseUp: this.onMouseUp
+    });
+
+    this.rubberband = new MagneticPolyline(x, y, this.g, this.env);
+  }
+
+  stop = () => {
+    if (this.rubberband) {
+      this.rubberband.destroy();
+      this.rubberband = null;
+    }
+  }
+
+  onMouseMove = (x, y) => {
+
+  }
+
+  onMouseUp = () => {
+
+  }
+
+  get isDrawing() {
+    return this.rubberband != null;
+  }
+
+  // TODO 
+  createEditableShape = annotation => {
+    throw 'Coming soon';
+  }
+
+}
+
+MagneticPolylineTool.identifier = 'magnetic-polyline';
+
+MagneticPolylineTool.supports = annotation => {
+  // TODO
+}
