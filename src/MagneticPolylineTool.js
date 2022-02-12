34b759f35d3ee722961dbcf617999fdace25f1dd
Rainer Simon <aboutgeo@gmail.com>
2022-02-12 10:04:52 +0100

    Started moving things into a background worker

--- src/MagneticPolylineTool.js
+++ src/MagneticPolylineTool.js
@@ -4,6 +4,7 @@ import Tool from '@recogito/annotorious/src/tools/Tool';
 
 // import jsfeat from 'jsfeat';
 
+/*
 import MagneticPolyline from './MagneticPolyline';
 
 const chunk = (array, size) => {	
@@ -18,6 +19,8 @@ const chunk = (array, size) => {
   return chunked_arr;	
 }
 
+*/
+
 export default class MagneticPolylineTool extends Tool {
 
   constructor(g, config, env) {
@@ -26,64 +29,16 @@ export default class MagneticPolylineTool extends Tool {
     // The 'rubberband' magnetic polyline
     this.rubberband = null;
 
-    // this.queue = new Worker(new URL('./WorkerQueue.js', import.meta.url));
+    this.queue = new Worker(new URL('./WorkerQueue.js', import.meta.url));
 
-    // Init OpenCV magic
+    this.queue.onmessage = msg => {
+      console.log('queue response', msg);
+    };
+    
+    /* Init OpenCV magic
     cv.onRuntimeInitialized = () => {
-      console.log(cv);
-      /*
-      const w = this.env.image.naturalWidth;
-      const h = this.env.image.naturalHeight;
-
-      const canvas = document.createElement('canvas');
-      canvas.width = w;
-      canvas.height = h;
-
-      const ctx = canvas.getContext('2d');
-      ctx.drawImage(this.env.image, 0, 0);
-
-      ctx.fillStyle = 'red';
-
-      const img = ctx.getImageData(0, 0, w, h);
-      
-      const gray_img = new jsfeat.matrix_t(w, h, jsfeat.U8_t | jsfeat.C1_t);
-      const code = jsfeat.COLOR_RGBA2GRAY;
-      jsfeat.imgproc.grayscale(img.data, w, h, gray_img, code);
-
-      const threshold = 20;
-      jsfeat.fast_corners.set_threshold(threshold);
-
-      const corners = [];
-      const border = 3;
- 
-      for (let i = 0; i<gray_img.cols * gray_img.rows; ++i) {
-        corners[i] = new jsfeat.keypoint_t(0,0,0,0);
-      }
-
-      const count = jsfeat.fast_corners.detect(gray_img, corners, border);
-
-      for (let i = 0; i<count; i++) {
-        const {x,y} = corners[i];
-        ctx.fillRect(x,y,4,4);       
-      }
-
-      document.body.appendChild(canvas);
       */
 
-      /*
-
-      const corners = [];
-      const border = 3;
- 
-// you should use preallocated keypoint_t array
-for(var i = 0; i < img.cols*img.rows, ++i) {
-    corners[i] = new jsfeat.keypoint_t(0,0,0,0);
-}
- 
-// perform detection
-// returns the amount of detected corners
-var count = jsfeat.fast_corners.detect(img:matrix_t, corners:Array, border = 3);
-*/
       // const img = cv.imread('map');
       // 
       // // const img = new cv.Mat();
@@ -92,6 +47,7 @@ var count = jsfeat.fast_corners.detect(img:matrix_t, corners:Array, border = 3);
       // const dst = new cv.Mat();
 			// cv.cvtColor(img, dst, cv.COLOR_RGB2GRAY.value, 0);
       
+      /*
       const img = cv.imread('map');
       const greyscale = new cv.Mat();
       // You can try more different parameters
@@ -106,6 +62,7 @@ var count = jsfeat.fast_corners.detect(img:matrix_t, corners:Array, border = 3);
       const k = 0.05;
       */
 
+      /*
       let kp = new cv.KeyPointVector();
 
       const fast = new cv.FastFeatureDetector(); // null, null, cv.FastFeatureDetector_TYPE_5_8);
@@ -126,6 +83,7 @@ var count = jsfeat.fast_corners.detect(img:matrix_t, corners:Array, border = 3);
         ctx.arc(x, y, 2, 0, 2 * Math.PI);
         ctx.fill();
       }
+      */
 
       /*
 
@@ -134,23 +92,25 @@ var count = jsfeat.fast_corners.detect(img:matrix_t, corners:Array, border = 3);
       this.scissors.setGradientMagnitudeMaxLimit(200);
       this.scissors.applyImage(img);
       */
-    }
+    // }
   }
 
   startDrawing = (x, y) => {
-    this.g.parentNode.style.cursor = 'none';
+    const w = this.env.image.naturalWidth;
+    const h = this.env.image.naturalHeight;
 
-    console.time('Building map');
+    const canvas = document.createElement('canvas');
+    canvas.width = w;
+    canvas.height = h;
 
-    // console.log(cv.imread(this.env.image));
+    const ctx = canvas.getContext('2d');
+    ctx.drawImage(this.env.image, 0, 0);
     
-    /*
     this.queue.postMessage({
       action: 'buildMap',
       x, y,
-      image: Array.from(cv.imread(this.env.image).data32S)
+      image: ctx.getImageData(0, 0, w, h)
     });
-    */
     
     /*
     this.queue.onmessage = msg => {
@@ -160,6 +120,7 @@ var count = jsfeat.fast_corners.detect(img:matrix_t, corners:Array, border = 3);
 
     // this.scissors.buildMap(new cv.Point(x, y));
     
+    /*
     console.timeEnd('Building map');
    
     this.attachListeners({
@@ -168,6 +129,7 @@ var count = jsfeat.fast_corners.detect(img:matrix_t, corners:Array, border = 3);
     });
 
     this.rubberband = new MagneticPolyline([x, y], this.scissors, this.g, this.env);
+    */
   }
 
   stop = () => {
--- src/WorkerQueue.js
+++ src/WorkerQueue.js
@@ -1,5 +1,31 @@
 import cv from '@techstark/opencv-js';
 
-self.onmessage = function(msg) {
-  console.log(msg, cv);
+const DEFAULT_FAST_THRESHOLD = 20;
+
+const detectCorners = (imageData, optThreshold) => {
+  const img = cv.matFromImageData(imageData);
+  const kp = new cv.KeyPointVector();
+
+  const fast = new cv.FastFeatureDetector();
+  fast.setThreshold(optThreshold || DEFAULT_FAST_THRESHOLD);
+  fast.detect(img, kp);
+
+  const arr = [];
+
+  for (let i=0; i<kp.size(); i++) {
+    const { x, y } = kp.get(i).pt;
+    arr.push(x);
+    arr.push(y);
+  }
+
+  return arr;
+}
+
+self.onmessage = function({data}) {
+  const { action, x, y, image } = data;
+
+  if (action === 'buildMap') {
+    const keypoints = new Uint16Array(detectCorners(image)).buffer;
+    self.postMessage({ type: 'keypoints', result: keypoints }, [ keypoints ]);
+  }
 };
